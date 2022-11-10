238daeed32d361ee109f737112337c2bc9f93b05
Cristian Sanchez <cristian.manu7@gmail.com>
2022-11-10 09:28:26 -0700

    image avatar

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Add from '../img/addAvatar.png'
 const Register = () => {
   return (
     <div className="formContainer">
@@ -10,8 +10,12 @@ const Register = () => {
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="email" placeholder="password" />
+          {/* innter style for file */}
           <input style={{ display: 'none' }} type="file" id="file" />
-          <label htmlFor="file">asdfg</label>
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an Avatar</span>
+          </label>
           <button>Sign up</button>
         </form>
         <p>You do have an account? Login</p>
