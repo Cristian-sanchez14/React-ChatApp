ef048531d1877aee7f6c49d483b6372a246e7a4c
Cristian Sanchez <cristian.manu7@gmail.com>
2022-11-10 23:12:52 -0700

    Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Home from './pages/Home'
 import './style.scss'
 
 function App() {
-  return <Login />
+  return <Home />
 }
 
 export default App
